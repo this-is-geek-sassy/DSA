c56f757f234fc16d7b28d409fcb55130cf91b954
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2026-05-29 13:42:37 +0530

    finally solved

--- striver/java/ThreeSum.java
+++ striver/java/ThreeSum.java
@@ -5,6 +5,45 @@ import java.util.*;
 
 public class ThreeSum {
 
+    public static List<List<Integer>> threeSum2 (int[] nums) {
+
+        Arrays.sort(nums);
+        List <List<Integer>> ans = new ArrayList<>();
+        
+        for (int i=0; i < nums.length-2; i++) {
+
+            if (i > 0 && nums[i-1] == nums[i])
+                continue;
+
+            int left = i+1;
+            int right = nums.length-1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left-1])
+                        left++;
+
+                    while (left < right && nums[right] == nums[right+1])
+                        right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
     public static List<List<Integer>> threeSum(int[] nums) {
 
         Arrays.sort(nums);
@@ -77,7 +116,8 @@ public class ThreeSum {
             nums[i] = Integer.parseInt(input_arr[i]);
         }
 
-
+        List<List<Integer>> ans = threeSum2(nums);
+        System.out.println(ans);
         sc.close();
     }
 }
