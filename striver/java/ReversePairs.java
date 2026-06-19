4f0ccde68db710fd605d93b797ccd5acc1197664
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2026-06-20 02:34:46 +0530

    solved

--- striver/java/ReversePairs.java
+++ striver/java/ReversePairs.java
@@ -1,10 +1,9 @@
 
+// link: https://leetcode.com/problems/reverse-pairs/description/
+
 import java.util.Scanner;
 
-
-// link: https://www.geeksforgeeks.org/problems/inversion-of-array-1587115620/1
-
-public class CountInversionsMerge {
+public class ReversePairs {
 
     // public static int counter = 0;
 
@@ -27,14 +26,24 @@ public class CountInversionsMerge {
 
         int[] newArr = new int[end - start + 1];
         int invCount = 0;
-        int i = 0, j = start, k = mid+1;
+        int j = mid+1;
+
+        for (int a=start; a <= mid; a++) {
+            while (j <= end && (long)nums[a] > 2L*nums[j]) {
+                j++;
+            }
+            invCount += j - (mid + 1);
+        }
+        int i = 0;
+        j = start;
+        int k = mid+1;
 
         while (j <= mid && k <= end) {
             if (nums[j] <= nums[k]) {
                 newArr[i] = nums[j];
                 j++;
             } else {
-                invCount += (mid + 1) -j;
+                // invCount += (mid + 1) -j;
                 newArr[i] = nums[k];
                 k++;
             }
@@ -61,34 +70,66 @@ public class CountInversionsMerge {
         return invCount;
     }
 
-    public static int inversionCount(int arr[]) {
-        // Code Here
-        
-        // int _jmp = 1;
-        // for (int j = 1; 2*j < arr.length; j++) {
+    public static int merge_2 (int[] nums, int start, int mid, int end) {
 
-        //     for (int i=0; i< arr.length; i += (2*j)) {
-        //         int k = i + _jmp;
-                
-        //     }
-        // }
-        return merge_sort(arr, 0, arr.length-1);
+        int[] newArr = new int[end - start + 1];
+        int invCount = 0;
+        int i = newArr.length-1, j = mid, k = end;
+
+        while (j >= start && k > mid) {
+            if (nums[j] < nums[k]) {
+                newArr[i] = nums[k];
+                k--;
+            } else {
+                if (nums[j] > 2*nums[k]) {
+                    invCount += (mid + 1) - j;
+                    System.out.println("j = " + j + " k = " + k);
+                }
+                newArr[i] = nums[j];
+                j--;
+            }
+            // System.out.println("i = " + i);
+            // System.out.println("newArr[i]: " + newArr[i]);
+            i--;
+        }
+        if (j < start) {
+            for (; i >=0; i--) {
+                newArr[i] = nums[k];
+                k--;
+            }
+        } else if (k <= mid) {
+            for (; i >= 0; i--) {
+                newArr[i] = nums[j];
+                j--;
+            }
+        }
+        i = 0; j = 0;
+        for (i = start; i <= end; i++) {
+            nums[i] = newArr[j];
+            j++;
+        }
+        return invCount;
     }
-    
+    public static int reversePairs(int[] nums) {
+        return merge_sort(nums, 0, nums.length-1);
+    }
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         input = input.trim().substring(1, input.length()-1);
-        String[] ip_arr = input.split(", ");
+        String[] ip_arr = input.split(",");
         int[] arr = new int[ip_arr.length];
 
         for (int i=0; i<ip_arr.length; i++) {
             arr[i] = Integer.parseInt(ip_arr[i]);
         }
         
-        int ans = inversionCount(arr);
+        int ans = reversePairs(arr);
         System.out.println(ans);
+        System.out.println("nums: ");
+        for (int e : arr) {
+            System.out.print(e + " ");
+        }
         sc.close();
     }
 }
