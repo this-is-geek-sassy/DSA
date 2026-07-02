e0ab3beedaf9806b7e79f570997c71e3981bc494
Saswata Mishra <cs24mtech12001@iith.ac.in>
2026-07-02 12:19:48 +0530

    solved

---
+++ striver/java/LargestOddNumberInString.java
@@ -0,0 +1,16 @@
+
+// link: https://leetcode.com/problems/largest-odd-number-in-string/
+
+public class LargestOddNumberInString {
+    public String largestOddNumber(String num) {
+        for (int i=num.length()-1; i>=0 ;i--) {
+            if (((num.charAt(i) - '0') & 1) == 1) {
+                return num.substring(0, i+1);
+            }
+        }
+        return "";
+    }
+    public static void main(String[] args) {
+        
+    }
+}
