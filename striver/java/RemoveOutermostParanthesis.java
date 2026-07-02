be85dc0b7bee88c22dea7d6cb8d1deec31d039a8
Saswata Mishra <cs24mtech12001@iith.ac.in>
2026-07-02 10:58:01 +0530

    better runime

--- striver/java/RemoveOutermostParanthesis.java
+++ striver/java/RemoveOutermostParanthesis.java
@@ -1,38 +1,43 @@
 
 // link: https://leetcode.com/problems/remove-outermost-parentheses/description/
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
 import java.util.Scanner;
 
 public class RemoveOutermostParanthesis {
     public static String removeOuterParentheses(String s) {
-        ArrayList<StringBuilder> decomposition = new ArrayList<>();
-        Deque<Character> st = new ArrayDeque<>();
+        // ArrayList<StringBuilder> decomposition = new ArrayList<>();
+        // Deque<Character> st = new ArrayDeque<>();
+        int count = 0;
+        StringBuilder ans = new StringBuilder();
         StringBuilder part = new StringBuilder();
 
         for (int i=0; i<s.length(); i++) {
             if (s.charAt(i) == '(') {
-                st.push('(');
-                part += "(";
+                // st.push('(');
+                count++;
+                part.append("(");
             } else {
-                st.pop();
-                part += ")";
+                // st.pop();
+                count--;
+                part.append(")");
             }
-            if (st.isEmpty()) {
-                decomposition.add(part);
-                part = "";
+            if (count == 0) {
+                // decomposition.add(part);
+                part.deleteCharAt(0);
+                part.deleteCharAt(part.length()-1);
+                ans.append(part);
+                part = new StringBuilder();
             }
         }
+        return ans.toString();
         // int i = 0;
-        String ans = "";
-        for (String str : decomposition) {
-            // System.out.print(str + " ");
-            String temp = str.substring(1, str.length()-1);
-            ans += temp;
-        }
-        return ans;
+        // String ans = "";
+        // for (StringBuilder str : decomposition) {
+        //     // System.out.print(str + " ");
+        //     String temp = str.substring(1, str.length()-1);
+        //     ans += temp;
+        // }
+        // return ans;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
