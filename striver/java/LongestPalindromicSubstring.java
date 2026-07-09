31b7e65af3b19658cc88c8aade2191122473a38d
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2026-07-09 13:44:44 +0530

    improved time complexity

--- striver/java/LongestPalindromicSubstring.java
+++ striver/java/LongestPalindromicSubstring.java
@@ -33,11 +33,59 @@ public class LongestPalindromicSubstring {
         }
         return sb;
     }
+
+    public static String longestPalindromeAlt(String s) {
+
+        int largest_len=0;
+        String ans = "";
+        // odd centers
+        for (int i=0; i<s.length(); i++) {
+            Character center = s.charAt(i);
+            int left = i-1, right = i+1;
+            // StringBuilder palin = new StringBuilder(center.toString());
+
+            while (left>=0 && right<=s.length()-1) {
+                if (s.charAt(left) == s.charAt(right)) {
+                    // palin.append(s.charAt(right));
+                    // palin.insert(0, s.charAt(left));
+                    left--;
+                    right++;
+                } else {
+                    break;
+                }
+            }
+            if ((--right-++left+1) > largest_len && right <= s.length()-1 && left>=0) {
+                ans = s.substring(left, right+1);
+                largest_len = (right-left+1);
+            }
+            // System.out.println("i = " + i);
+        }
+        // System.out.println("largest_len: " + largest_len);
+        // even centers
+        for (int i=0; i<s.length()-1; i++) {
+            int left = i, right = i+1;
+            // Character leftchar = s.charAt(left), rightchar = s.charAt(right);
+            // StringBuilder palin = new StringBuilder();
+            while (left>=0 && right<=s.length()-1) {
+                if (s.charAt(left) != s.charAt(right))
+                    break;
+                // palin.append(s.charAt(right));
+                // palin.insert(0, s.charAt(left));
+                left--;
+                right++;
+            }
+            if ((--right-++left+1) > largest_len && right <= s.length()-1 && left>=0) {
+                ans = s.substring(left, right+1);
+                largest_len = (right-left+1);
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        String ans = longestPalindrome(input);
+        String ans = longestPalindromeAlt(input);
         System.out.println(ans);
         sc.close();
     }
