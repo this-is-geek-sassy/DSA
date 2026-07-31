8bf4b23caf9452bc395174565efa4d74d3a6e3c8
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2026-07-31 14:20:39 +0530

    incomplete sort

---
+++ striver/java/LinkedListSort.java
@@ -0,0 +1,76 @@
+
+// link: https://leetcode.com/problems/sort-list/
+
+import java.util.Scanner;
+
+public class LinkedListSort {
+
+    private static ListNode merge (ListNode head1, ListNode head2) {
+
+        ListNode head = new ListNode(Integer.MIN_VALUE), temp1 = head1, temp2 = head2, temp = head;
+
+        while (temp1 != null && temp2 != null)
+        {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+                temp = temp.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+                temp = temp.next;
+            }
+        }
+        temp.next = (temp1 != null) ? temp1 : temp2;
+        return head.next;
+    }
+    
+    public static ListNode MergeSort(ListNode head, ListNode tail) {
+
+        if (head == tail)
+            return head;
+        // mid calculation:
+        ListNode slow = head, fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            if (fast.next.next.next == null)
+                break;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = null;
+        ListNode newHead = MergeSort(head, slow);
+        ListNode newHead2 = MergeSort(slow.next, tail);
+
+        return merge(newHead, newHead2);
+
+    }
+    public static ListNode sortList(ListNode head) {
+        
+        // tail pointer: 
+        ListNode tail = head;
+        while (tail.next != null) 
+            tail = tail.next;
+        return MergeSort(head, tail);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String ip = sc.nextLine();
+        String[] ip_arr = ip.trim().substring(1, ip.length()-1).split(",");
+        int[] arr = new int[ip_arr.length];
+        int i = 0;
+
+        for (String s: ip_arr) {
+            arr[i++] = Integer.parseInt(s);
+        }
+
+        ListNode head = SinglyLinkedList.arrayToLL(arr);
+        // int n = sc.nextInt();
+        
+        head = sortList(head);
+        SinglyLinkedList.printList(head);
+
+        sc.close();
+    }
+}
