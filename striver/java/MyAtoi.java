9877ad1f06bd3ff95df6db91310fdf328444c0c7
Saswata Mishra <cs24mtech12001@iith.ac.in>
2026-08-03 14:33:27 +0530

    solved

--- striver/java/MyAtoi.java
+++ striver/java/MyAtoi.java
@@ -4,21 +4,40 @@ import java.util.Scanner;
 
 public class MyAtoi {
 
-    private static int atoiRecursive(String s, int idx, int sign) {
+    private static final long[] POW10 = new long[19];
+    static {
+        POW10[0] = 1;
+        for (int i = 1; i < POW10.length; i++) {
+            POW10[i] = POW10[i - 1] * 10;
+        }
+    }
+
+    private static long pow10(int n) {
+        return POW10[n];
+    }
+
+    private static int atoiRecursive(String s, int sign) {
 
         if (s.isEmpty())
             return 0;
-        if (idx == 0)
-            return (s.charAt(0) - '0');
-        int num = atoiRecursive(s.substring(1), idx - 1, sign);
 
-        long temp = ((s.charAt(0) - '0') * (long) Math.pow(10, idx)) + num;
-        if (temp > Integer.MAX_VALUE)
-            return Integer.MAX_VALUE;
-        else if (sign == -1 && temp > 2147483648L)
+        int num = atoiRecursive(s.substring(1), sign);
+
+        if (num == Integer.MIN_VALUE || num == Integer.MAX_VALUE)
+            return num;
+
+        long num2 = (long)(s.charAt(0) - '0') * pow10(s.length() - 1);
+        long temp = num + num2;
+
+        if (sign == -1 && temp >= (long) Integer.MAX_VALUE + 1) {
             return Integer.MIN_VALUE;
-        else
-            return (int) temp;
+        }
+        else if (temp > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        else {
+            return (int)temp;
+        }
     }
 
     public static int myAtoi(String s) {
@@ -38,19 +57,24 @@ public class MyAtoi {
                 break;
         }
         s = s.substring(0, i);
-        System.out.println(i);
-        System.out.println(s);
+        // System.out.println(i);
+        // System.out.println(s);
         // return 1<<3; // 2^3
         if (i == 0)
             return 0;
 
+        // zero omission part
         for (i = 0; i < s.length(); i++) {
             if (s.charAt(i) != '0')
                 break;
         }
         s = s.substring(i);
-        System.out.println("s = " + s);
-        int ans = atoiRecursive(s, s.length() - 1, isNegative);
+        // System.out.println("s = " + s);
+        if (isNegative == -1 && s.length() > 10)
+            return Integer.MIN_VALUE;
+        else if (s.length() > 10) 
+            return Integer.MAX_VALUE;
+        int ans = atoiRecursive(s, isNegative);
 
         return (ans < 0) ? ans : ans*isNegative;
     }
@@ -61,7 +85,7 @@ public class MyAtoi {
         // System.out.println(Integer.MIN_VALUE);
         // System.out.println(Integer.MAX_VALUE);
         String input = sc.nextLine();
-        System.out.println(myAtoi(input));
+        System.out.println("Final ans = " + myAtoi(input));
         sc.close();
     }
 }
