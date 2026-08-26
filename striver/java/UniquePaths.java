8619025c8dda1429088ff89326fb9a9b4f20f50b
Saswata Mishra <cs24mtech12001@iith.ac.in>
2026-08-26 15:51:26 +0530

    solved with tabulation

--- striver/java/UniquePaths.java
+++ striver/java/UniquePaths.java
@@ -1,5 +1,4 @@
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 // link: https://leetcode.com/problems/unique-paths/
@@ -39,14 +38,31 @@ public class UniquePaths {
         return memory[m][n];
     }
 
-    
+    private static int helper2 (int m, int n) {
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
 
     private static int uniquePaths (int m, int n) {
-        int[][] memory = new int[m][n];
-        for (int[] i: memory) {
-            Arrays.fill(i, -1);
-        }
-        return helper(m-1, n-1, memory);
+        // int[][] memory = new int[m][n];
+        // for (int[] i: memory) {
+        //     Arrays.fill(i, -1);
+        // }
+        // return helper(m-1, n-1, memory);
+        return helper2(m, n);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
