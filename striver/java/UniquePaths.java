cb08602bc3cf3ec3962a10f44260b32388acba38
Saswata Mishra <cs24mtech12001@iith.ac.in>
2026-08-26 15:30:42 +0530

    solved with DP & combinatorics

---
+++ striver/java/UniquePaths.java
@@ -0,0 +1,60 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// link: https://leetcode.com/problems/unique-paths/
+
+public class UniquePaths {
+
+    private static int cominatoricsSoln (int m, int n) {
+
+        // C(m+n-2, n-1) or C(m+n-2, m-1)
+        int N = m + n - 2;
+        int K = Math.min(m - 1, n - 1);
+
+        long result = 1;
+
+        for (int i = 1; i <= K; i++) {
+            result = result * (N - K + i) / i;
+        }
+
+        return (int) result;
+    }
+
+    private static int helper (int m, int n, int[][] memory) {
+
+        if (m==0 && n==0) {
+            return 1;
+        }
+        if (memory[m][n] != -1) {
+            return memory[m][n];
+        }
+        if (m==0) {
+            return helper(0, n-1, memory);
+        }
+        if (n==0) {
+            return helper(m-1, 0, memory);
+        }
+        memory[m][n] = helper(m-1, n, memory) + helper(m, n-1, memory);
+        return memory[m][n];
+    }
+
+    
+
+    private static int uniquePaths (int m, int n) {
+        int[][] memory = new int[m][n];
+        for (int[] i: memory) {
+            Arrays.fill(i, -1);
+        }
+        return helper(m-1, n-1, memory);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+
+        sc.close();
+    }
+}
