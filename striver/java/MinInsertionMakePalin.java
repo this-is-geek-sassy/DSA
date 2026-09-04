5615fbe4aafb134cf41ca88838a285503930f2a2
Saswata Mishra <cs24mtech12001@iith.ac.in>
2026-09-04 17:49:48 +0530

    found shortcut; did not need to solve

---
+++ striver/java/MinInsertionMakePalin.java
@@ -0,0 +1,6 @@
+public class MinInsertionMakePalin {
+    public static void main(String[] args) {
+        // len(String) - len(LPS)
+        // https://chatgpt.com/share/6a9ab6da-1460-83ee-9f3e-b518cc1d75f1
+    }
+}
